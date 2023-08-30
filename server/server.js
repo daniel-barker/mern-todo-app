2dbd9814526f31019cc17946b6a54b49ff70d48b
dan barker <dbarker16@gmail.com>
2023-08-30 14:29:06 -0400

    push

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import index from './routes/index.js';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-import mongoose from 'mongoose';
 
 mongoose.connect('mongodb://localhost:27017/todoDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+
+app.use(express.json());
+app.use("/", index);
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
